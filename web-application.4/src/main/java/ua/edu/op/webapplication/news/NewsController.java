1f236c98b5cea5b71cbcd04edd8558e4aa4601c2
daniil-svetashov <oodessa13@gmail.com>
2022-05-30 17:10:11 +0300

    works

--- web-application.4/src/main/java/ua/edu/op/webapplication/news/NewsController.java
+++ web-application.4/src/main/java/ua/edu/op/webapplication/news/NewsController.java
@@ -4,13 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import ua.edu.op.webapplication.image.Image;
 
 import javax.validation.Valid;
 import java.io.IOException;
